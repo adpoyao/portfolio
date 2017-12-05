5e8b5502b84476a8767c25702b11cb1f5739f470
Eddie Yao <adpoyao@gmail.com>
2017-12-05 15:07:25 -0800

    CSS touch up: afternoon

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 'use strict';
 
-// 550 vs 470 (diff 80)
-
-var mainNav = $('.nav-desktop');
+//LOCK NAV BAR WHEN SCROLLING
+const mainNav = $('.nav-desktop');
+const mainSlideshow = $('.header-slideshow-desktop');
 $(window).scroll(function() {
   if ( $(this).scrollTop() > 550 ) {
     mainNav.addClass('main-nav-scrolled');
+    mainSlideshow.addClass('hidden');
   }
   else {
     mainNav.removeClass('main-nav-scrolled');
+    mainSlideshow.removeClass('hidden');
   }
 });
+
+// //FADE IN INTRO PICTURE (DESKTOP VIEW)
+// function fadeIn(obj) {
+//   $(obj).fadeIn(2000);
+// }
+
+//SEAMLESS TRANSITION TO #LINKS
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 750, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
