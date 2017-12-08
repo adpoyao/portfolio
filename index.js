e89aa635734a14b6e7652a21e9ba1602a9301570
Eddie Yao <adpoyao@gmail.com>
2017-12-08 14:52:14 -0800

    added grid to webdev section

--- index.js
+++ index.js
@@ -57,7 +57,42 @@ $('a[href*="#"]')
     }
   });
 
-//SLIDE TEXTS
+//FADE: ABOUT
+$(document).ready(function(){
+  $('.divs div').each(function(e) {
+    if (e !== 0)
+      $(this).hide();
+  });
+  
+  $('#next').click(function(){
+    if ($('.divs div:visible').next().length !== 0)
+      $('.divs div:visible').fadeOut(function(){
+        $(this).next().fadeIn();
+      });
+    else {
+      $('.divs div:visible').fadeOut(function(){
+        $('.divs div:first').fadeIn();
+      });
+    }
+    return false;
+  });
+
+  $('#prev').click(function(){
+    if ($('.divs div:visible').prev().length !== 0)
+      $('.divs div:visible').fadeOut(function(){
+        $(this).prev().fadeIn();
+      });
+    else {
+      $('.divs div:visible').fadeOut(function(){
+        $('.divs div:last').fadeIn();
+      });
+    }
+    return false;
+  });
+});
+
+
+//FADE: WD-1
 $(document).ready(function(){
   $('.divs div').each(function(e) {
     if (e !== 0)
